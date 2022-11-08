9ae25204fdd53c652b7bc72dfb4d7c1dc7143f21
DATA <DATA@DESKTOP-FFAI8EK>
2022-11-08 11:09:34 +0100

    Ajoutez des fichiers projet.

---
+++ calculatrice/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace calculatrice
+{
+    partial class calculatrice
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTN5 = new System.Windows.Forms.Button();
+            this.BTN4 = new System.Windows.Forms.Button();
+            this.BTN3 = new System.Windows.Forms.Button();
+            this.BTN1 = new System.Windows.Forms.Button();
+            this.BTN2 = new System.Windows.Forms.Button();
+            this.BTN6 = new System.Windows.Forms.Button();
+            this.SIMA = new System.Windows.Forms.Button();
+            this.MOINS = new System.Windows.Forms.Button();
+            this.PLUS = new System.Windows.Forms.Button();
+            this.BTN7 = new System.Windows.Forms.Button();
+            this.BTN8 = new System.Windows.Forms.Button();
+            this.BTN9 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BTN5
+            // 
+            this.BTN5.Location = new System.Drawing.Point(137, 129);
+            this.BTN5.Name = "BTN5";
+            this.BTN5.Size = new System.Drawing.Size(75, 34);
+            this.BTN5.TabIndex = 0;
+            this.BTN5.Text = "5";
+            this.BTN5.UseVisualStyleBackColor = true;
+            this.BTN5.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BTN4
+            // 
+            this.BTN4.Location = new System.Drawing.Point(137, 89);
+            this.BTN4.Name = "BTN4";
+            this.BTN4.Size = new System.Drawing.Size(75, 36);
+            this.BTN4.TabIndex = 1;
+            this.BTN4.Text = "4";
+            this.BTN4.UseVisualStyleBackColor = true;
+            // 
+            // BTN3
+            // 
+            this.BTN3.Location = new System.Drawing.Point(218, 169);
+            this.BTN3.Name = "BTN3";
+            this.BTN3.Size = new System.Drawing.Size(75, 29);
+            this.BTN3.TabIndex = 2;
+            this.BTN3.Text = "3";
+            this.BTN3.UseVisualStyleBackColor = true;
+            this.BTN3.Click += new System.EventHandler(this.BTN3_Click);
+            // 
+            // BTN1
+            // 
+            this.BTN1.Location = new System.Drawing.Point(218, 89);
+            this.BTN1.Name = "BTN1";
+            this.BTN1.Size = new System.Drawing.Size(75, 34);
+            this.BTN1.TabIndex = 3;
+            this.BTN1.Text = "1";
+            this.BTN1.UseVisualStyleBackColor = true;
+            this.BTN1.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // BTN2
+            // 
+            this.BTN2.Location = new System.Drawing.Point(218, 129);
+            this.BTN2.Name = "BTN2";
+            this.BTN2.Size = new System.Drawing.Size(75, 34);
+            this.BTN2.TabIndex = 4;
+            this.BTN2.Text = "2";
+            this.BTN2.UseVisualStyleBackColor = true;
+            this.BTN2.Click += new System.EventHandler(this.BTN2_Click);
+            // 
+            // BTN6
+            // 
+            this.BTN6.Location = new System.Drawing.Point(137, 169);
+            this.BTN6.Name = "BTN6";
+            this.BTN6.Size = new System.Drawing.Size(75, 29);
+            this.BTN6.TabIndex = 5;
+            this.BTN6.Text = "6";
+            this.BTN6.UseVisualStyleBackColor = true;
+            // 
+            // SIMA
+            // 
+            this.SIMA.Location = new System.Drawing.Point(299, 136);
+            this.SIMA.Name = "SIMA";
+            this.SIMA.Size = new System.Drawing.Size(75, 29);
+            this.SIMA.TabIndex = 6;
+            this.SIMA.Text = "÷";
+            this.SIMA.UseVisualStyleBackColor = true;
+            this.SIMA.Click += new System.EventHandler(this.SIMA_Click);
+            // 
+            // MOINS
+            // 
+            this.MOINS.Location = new System.Drawing.Point(299, 96);
+            this.MOINS.Name = "MOINS";
+            this.MOINS.Size = new System.Drawing.Size(75, 34);
+            this.MOINS.TabIndex = 7;
+            this.MOINS.Text = "-";
+            this.MOINS.UseVisualStyleBackColor = true;
+            this.MOINS.Click += new System.EventHandler(this.MOINS_Click);
+            // 
+            // PLUS
+            // 
+            this.PLUS.Location = new System.Drawing.Point(299, 58);
+            this.PLUS.Name = "PLUS";
+            this.PLUS.Size = new System.Drawing.Size(75, 32);
+            this.PLUS.TabIndex = 8;
+            this.PLUS.Text = "+";
+            this.PLUS.UseVisualStyleBackColor = true;
+            this.PLUS.Click += new System.EventHandler(this.PLUS_Click);
+            // 
+            // BTN7
+            // 
+            this.BTN7.Location = new System.Drawing.Point(56, 89);
+            this.BTN7.Name = "BTN7";
+            this.BTN7.Size = new System.Drawing.Size(75, 36);
+            this.BTN7.TabIndex = 9;
+            this.BTN7.Text = "7";
+            this.BTN7.UseVisualStyleBackColor = true;
+            // 
+            // BTN8
+            // 
+            this.BTN8.Location = new System.Drawing.Point(56, 129);
+            this.BTN8.Name = "BTN8";
+            this.BTN8.Size = new System.Drawing.Size(75, 34);
+            this.BTN8.TabIndex = 10;
+            this.BTN8.Text = "8";
+            this.BTN8.UseVisualStyleBackColor = true;
+            this.BTN8.Click += new System.EventHandler(this.BTN8_Click);
+            // 
+            // BTN9
+            // 
+            this.BTN9.Location = new System.Drawing.Point(56, 169);
+            this.BTN9.Name = "BTN9";
+            this.BTN9.Size = new System.Drawing.Size(75, 29);
+            this.BTN9.TabIndex = 11;
+            this.BTN9.Text = "9";
+            this.BTN9.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(315, 20);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(218, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 28);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "0";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(299, 171);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 28);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "=";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(137, 58);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 28);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "C";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // calculatrice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 244);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.BTN9);
+            this.Controls.Add(this.BTN8);
+            this.Controls.Add(this.BTN7);
+            this.Controls.Add(this.PLUS);
+            this.Controls.Add(this.MOINS);
+            this.Controls.Add(this.SIMA);
+            this.Controls.Add(this.BTN6);
+            this.Controls.Add(this.BTN2);
+            this.Controls.Add(this.BTN1);
+            this.Controls.Add(this.BTN3);
+            this.Controls.Add(this.BTN4);
+            this.Controls.Add(this.BTN5);
+            this.Name = "calculatrice";
+            this.Text = "calculatrice";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTN5;
+        private System.Windows.Forms.Button BTN4;
+        private System.Windows.Forms.Button BTN3;
+        private System.Windows.Forms.Button BTN1;
+        private System.Windows.Forms.Button BTN2;
+        private System.Windows.Forms.Button BTN6;
+        private System.Windows.Forms.Button SIMA;
+        private System.Windows.Forms.Button MOINS;
+        private System.Windows.Forms.Button PLUS;
+        private System.Windows.Forms.Button BTN7;
+        private System.Windows.Forms.Button BTN8;
+        private System.Windows.Forms.Button BTN9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ calculatrice/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculatrice
+{
+    public partial class calculatrice : Form
+    {
+        double FirstNumber;
+        string Op;
+        public calculatrice()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "5";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "1";
+        }
+
+        private void BTN2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "2";
+        }
+
+        private void MOINS_Click(object sender, EventArgs e)
+        {
+            FirstNumber = Convert.ToDouble(textBox1.Text);           
+            Op = "-";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BTN3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "3";
+        }
+
+        private void BTN8_Click(object sender, EventArgs e)
+        {
+            
+            textBox1.Text = "8";
+        }
+
+        private void PLUS_Click(object sender, EventArgs e)
+        {
+            FirstNumber = Convert.ToDouble(textBox1.Text);
+            Op = "+";
+        }
+
+        private void SIMA_Click(object sender, EventArgs e)
+        {
+            FirstNumber = Convert.ToDouble(textBox1.Text);
+            Op = "/";
+        }
+    }
+}
